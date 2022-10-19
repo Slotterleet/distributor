45c2352809fe030470b8fbbe6aeaf7d4b9da747e
Slotterleet <62336673+Slotterleet@users.noreply.github.com>
2022-10-19 08:39:25 +0300

    Distributor language

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
+// Distributor
 Blocks.distributor.buildType = () => extend(Router.RouterBuild, Blocks.distributor, {
   canControl(){
     return true;
   }
 })
+
+// UI
+Events.on(EventType.ClientLoadEvent, e => {
+  Vars.ui.settings.addCategory("Distributor", "distributor", t => {
+    t.checkPref("distributor-language", false)
+  })
+})
+
+// Distributor Language
+if (Core.settings.getBool("distributor-language")) {
+  Core.bundle.debug("distributor");
+}
+
